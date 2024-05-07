e5799caa5d1a08ba07abe7f267afb954eec96086
janith chathuranga <it21250156@my.sliit.lk>
2024-05-07 17:42:41 +0530

    login resgister update

--- backend/auth-service/models/User.js
+++ backend/auth-service/models/User.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  mobileNumber: { type: String, required: true },
   password: { type: String, required: true },
-
   role: {
     type: String,
     enum: ['learner', 'instructor', 'admin'],
--- backend/user_profile/middleware/authMiddleware.js
+++ backend/user_profile/middleware/authMiddleware.js
@@ -1,21 +1,22 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
-function authenticateUser(req, res, next) {
-  const token = req.header('Authorization');
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
   if (!token) {
-    return res.status(401).json({ error: 'Authorization token not found.' });
+    return res.status(401).send('Unauthorized: No token provided');
   }
-  try {
-    const decoded = jwt.verify(token.split(' ')[1], config.jwtSecret);
-    console.log(decoded);
-    req.user = decoded.id;
+  jwt.verify(token, config.jwtSecret, (err, user) => {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).send('Unauthorized: Token expired');
+      }
+      return res.status(403).send('Forbidden: Invalid token');
+    }
+    req.user = user;
     next();
-  } catch (err) {
-    res.status(401).json({ error: 'Invalid authorization token.' });
-  }
+  });
 }
 
-module.exports = {
-  authenticateUser,
-};
+module.exports = authenticateToken;
