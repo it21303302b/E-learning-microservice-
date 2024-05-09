9128a571a25c5386c4f8f84dffc87d8235cf28f1
MalikaIT21277122 <IT21277122@my.sliit.lk>
2024-05-09 12:02:47 +0530

    2.2 Modified the course form

--- frontend/src/components/items/CourseForm.jsx
+++ frontend/src/components/items/CourseForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 import './CourseForm.css'; 
 
 const CourseForm = () => {
@@ -7,11 +8,14 @@ const CourseForm = () => {
   const [courseDescription, setCourseDescription] = useState('');
   const [lectureNotes, setLectureNotes] = useState('');
   const [coursePrice, setCoursePrice] = useState('');
+  const [instructorEmail, setInstructorEmail] = useState('');
   const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+  
+    const instructorId = localStorage.getItem('instructorID');
+  
     const course = {
       course_name: courseName,
       course_description: courseDescription,
@@ -19,18 +23,27 @@ const CourseForm = () => {
         lecture_notes: lectureNotes,
       },
       course_price: coursePrice,
+      instructor_email: instructorEmail, // Add instructor email
+      instructor_id: instructorId, // Add instructor ID from local storage
     };
-
+  
     try {
       await axios.post('http://localhost:4001/api/courses', course);
-      alert('Course successfully added');
-      // Optionally, you may redirect the user or perform other actions after successful submission
+      await Swal.fire({
+        title: 'Success!',
+        text: 'Course successfully added.',
+        icon: 'success',
+      });
+      // Redirect to DisplayCourses page after clicking OK
+      window.location.href = '/displayCourses';
     } catch (err) {
       alert('Error: Course not added');
       console.error(err);
       setError('Error: Course not added');
     }
   };
+  
+  
 
   return (
     <form className="create" onSubmit={handleSubmit}>
@@ -67,6 +80,14 @@ const CourseForm = () => {
         step="0.01"
         onChange={(e) => setCoursePrice(e.target.value)}
         value={coursePrice}
+        required
+      />
+
+      <label>Instructor Email:</label> {/* Add instructor email input field */}
+      <input
+        type="email"
+        onChange={(e) => setInstructorEmail(e.target.value)}
+        value={instructorEmail}
         required
       />
 
