2b86c7476828edb13073017b8e0251368bac0025
janith chathuranga <it21250156@my.sliit.lk>
2024-05-11 01:25:20 +0530

    login and signup selection pages ui

---
+++ frontend/src/components/common/LogoText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LogoText = () => {
+  return (
+    <div>
+      <p className="text-2xl font-extrabold text-black">
+        Course<span className="text-blue-900">.Mate</span>
+      </p>
+    </div>
+  )
+}
+
+export default LogoText
--- frontend/src/pages/displayitems.jsx
+++ frontend/src/pages/displayitems.jsx
@@ -1,19 +1,14 @@
-
-import { useEffect, useState } from 'react'
 import ItemDetails from '../components/items/coursePageCard'
-import ItemForm from '../components/items/ItemForm'
 import Layout from '../components/layout'
 
-const Displayitems = () =>{
-    return(
-        <div className="home">
-            <Layout>
-            <ItemDetails/>
-            </Layout>
-
-        </div>
-    )
+const Displayitems = () => {
+  return (
+    <div className="home">
+      <Layout>
+        <ItemDetails />
+      </Layout>
+    </div>
+  )
 }
 
 export default Displayitems
-
