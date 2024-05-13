8ec2fcb0cce6e6a5832c8e2c389a5412e84cd98d
MalikaIT21277122 <IT21277122@my.sliit.lk>
2024-05-13 22:30:42 +0530

    3.6 Course form modify

--- frontend/src/components/items/CourseForm.jsx
+++ frontend/src/components/items/CourseForm.jsx
@@ -137,9 +137,9 @@ const CourseForm = () => {
                   <label htmlFor="cDescription" className="block text-sm font-medium text-gray-900 dark:text-white">
                     Course Description:
                   </label>
-                  <input
+                  <textarea
                     id="cDescription"
-                    type="text"
+                    rows="3"
                     className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     onChange={(e) => setCourseDescription(e.target.value)}
                     value={courseDescription}
